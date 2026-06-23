c978348cbeeab3f9be0108142cf3a65da7b5efdd
Cirujanomolina <cirujanomolina@gmail.com>
2026-06-23 00:44:43 -0500

    pref

--- assets/index-BHQNg6gI.js
+++ assets/index-BHQNg6gI.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DbVhKKvc.js";/**
+import{c as a}from"./index-D6xcl45P.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
