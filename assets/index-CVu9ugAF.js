ef92f1be2606424529ae7e0750a179a447ccddd6
Cirujanomolina <cirujanomolina@gmail.com>
2026-08-19 16:48:12 -0500

    columnas

--- assets/index-CVu9ugAF.js
+++ assets/index-CVu9ugAF.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CMqHHUu5.js";/**
+import{c as a}from"./index-CQeBYYjJ.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
