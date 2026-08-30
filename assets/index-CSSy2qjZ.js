cff596ff4f959b9a55b3751f5f9fc9ff731fb47c
Cirujanomolina <cirujanomolina@gmail.com>
2026-08-30 18:22:09 -0500

    perfiles

--- assets/index-CSSy2qjZ.js
+++ assets/index-CSSy2qjZ.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B5UfF4mO.js";/**
+import{c as a}from"./index-CIAVu2EW.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
