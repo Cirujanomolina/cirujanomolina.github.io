7764cd5d2350a66e6f6a317ee71d8ab3c829075b
Cirujanomolina <cirujanomolina@gmail.com>
2026-09-08 23:15:20 -0500

    formulas

--- assets/index-DCwyEwGd.js
+++ assets/index-DCwyEwGd.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-HB8xpFI6.js";/**
+import{c as a}from"./index-CABEfGUL.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
